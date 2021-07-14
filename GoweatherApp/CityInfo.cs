e722b5f556b58e8f2b3b4b57af90a5f259f969a5
Mongol <mongol22102@hotmail.com>
2021-07-14 20:25:53 +0300

    Добавьте файлы проекта.

---
+++ GoweatherApp/CityInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoweatherApp
+{
+    public class CityInfo
+    {
+        public string Temperature { get; set; }
+    }
+}
---
+++ GoweatherApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GoweatherApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://goweather.herokuapp.com/weather/Kyiv");
+                response.EnsureSuccessStatusCode();
+                string responseT = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseT);
+                CityInfo ci = JsonConvert.DeserializeObject<CityInfo>(responseT);
+                Console.WriteLine($"Температура в Киеве: {ci.Temperature}");
+
+
+                response = await client.GetAsync("https://goweather.herokuapp.com/weather/Odesa");
+                response.EnsureSuccessStatusCode();
+                responseT = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseT);
+                ci = JsonConvert.DeserializeObject<CityInfo>(responseT);
+                Console.WriteLine($"Температура в Одессе: {ci.Temperature}");
+
+                Console.ReadKey();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+        }
+    }
+}
